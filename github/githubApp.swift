6ea2f8fb27803b154a3ea390e1e1000d45d43371
Jiwon <jiwonhwang0716@gmail.com>
2023-07-14 23:16:40 +0900

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Jiwon Hwang on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
